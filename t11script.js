e625da76efadbefc87a0ac885ef0eb90cca573ea
becket <becket.bowes@gmail.com>
2022-05-07 20:49:44 -0400

    initial commit

---
+++ t11script.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 24
+  const elevenminuteRatio = (hoursRatio) * 11
+  const elevensecondRatio = (elevenminuteRatio) * 121
+  setRotation(minuteHand, elevenminuteRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360 + 180)
+}
+
+setClock()
